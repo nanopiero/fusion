8fd0b49342250df730c64983ac996a5560f1f834
nanopiero <136181914+nanopiero@users.noreply.github.com>
2024-06-03 15:06:34 +0200

    coquille matplotlib

--- utile_fusion.py
+++ utile_fusion.py
@@ -11,7 +11,7 @@ import os
 import torch
 import torch.nn.functional as F
 import numpy as np
-import matplotib.pyplot as plt
+import matplotlib.pyplot as plt
 
 ##############################
 ########## with JIT ##########
